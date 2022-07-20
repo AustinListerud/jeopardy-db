a56ebd96457b0cab6a6c55ed59f6870e1d219d3e
Austin Listerud <austinlisterud@gmail.com>
2022-07-20 15:03:33 -0500

    Convert node routing to Express

--- jeopardy-app.js
+++ jeopardy-app.js
@@ -2,10 +2,23 @@ const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database('./jarchive.db');
 
-let http = require('http');
-let url = require('url');
-let fs = require('fs');
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.listen(port, () => {});
+
+/*
 http.createServer(function (req, res) {
   let q = url.parse(req.url, true);
   let filename = "." + q.pathname;
@@ -19,6 +32,7 @@ http.createServer(function (req, res) {
     return res.end();
   });
 }).listen(8080); 
+*/
 
 let query = 'aqaba';
 
